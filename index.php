9ee5c0c3f961200f09ae62f39bfd9a1678bff164
Namurinda Alex <namurindaalex43@gmail.com>
2025-09-14 11:50:50 +0300

    modal h5

--- index.php
+++ index.php
@@ -529,7 +529,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="packageModalLabel">Enter phone number to make payment</h5>
+                    <h5 class="modal-title" id="packageModalLabel">Enter your phone number to make payment</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
